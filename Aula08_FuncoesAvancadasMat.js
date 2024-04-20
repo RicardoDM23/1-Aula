81e35a638ec4d183dd91530620ae38163f6868c8
RicardoDM23 <ricardo.dm23@gmail.com>
2024-04-20 14:20:26 -0300

    Aula08 ajustes

--- Aula08_FuncoesAvancadasMat.js
+++ Aula08_FuncoesAvancadasMat.js
@@ -11,5 +11,5 @@ let v = Math.round(10.500) //0,5 pra 11 e a baixo pra 10
 console.log(v)
 let c = Math.round(10.490) //0,5 pra 11 e abaixo pra 10
 console.log(c)
-let r = Math.random() // retorna um numero semi aleatorio de 1 a 100
+let r = Math.random() // retorna um numero semi aleatorio de 1 a 10
 console.log(r)
--- ExAula08_FunAvDatas.js
+++ ExAula08_FunAvDatas.js
@@ -1,15 +1,15 @@
 /*
-* Fazer: encontra a quantidade de milisegundos entre duas datas
+* Fazer: encontra a quantidade de milissegundos entre duas datas
 * Ricardo D. Muenchen 20/04/2024
 */
 
 let dataInicial = new Date();
 let dataFinal = new Date();
-dataInicial.getDate();
-dataFinal.setDate(dataFinal.getDate() + 500);
+dataInicial.getDate(); // dia atual
+dataFinal.setDate(dataFinal.getDate() + 500); // dia atual + 500 dias
 
-console.log(dataInicial.toString(), '-Qtd milisegundos-' , dataInicial.getTime());
-console.log(dataFinal.toString(), '-Qtd milisegundos-', dataFinal.getTime());
+console.log(dataInicial.toString(), '-Qtd milissegundos-' , dataInicial.getTime());
+console.log(dataFinal.toString(), '-Qtd milissegundos-', dataFinal.getTime());
 
-let milisegundosEntreDatas = Math.abs(dataInicial.getTime() - dataFinal.getTime());
-console.log('Quantidade de milisegundos entre as duas datas: ', milisegundosEntreDatas);
+let milissegundosEntreDatas = Math.abs(dataInicial.getTime() - dataFinal.getTime());
+console.log('Quantidade de ms entre as duas datas: ', milissegundosEntreDatas);
